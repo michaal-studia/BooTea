bb1f6604a60e180a3fb775c36e6dcefedac17a32
babiseq2 <babirecki552@gmail.com>
2025-04-09 02:05:21 +0200

    Poprawiono przycisk wyjścia
    Można teraz zatrzymać grę również w edytorze

--- BooTea-game/Assets/QuitGame.cs
+++ BooTea-game/Assets/QuitGame.cs
@@ -4,9 +4,12 @@ public class QuitGame : MonoBehaviour
 {
     public void Quit()
     {
-        // Dzia³a tylko po zbudowaniu gry (nie zadzia³a w edytorze)
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false; // Zatrzymuje grï¿½ w edytorze
+#endif
+
         Application.Quit();
-        Debug.Log("Gra zosta³a zakoñczona.");
+        Debug.Log("Gra zostaï¿½a zakoï¿½czona.");
     }
 }
 
