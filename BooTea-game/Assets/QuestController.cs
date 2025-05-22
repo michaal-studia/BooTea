c8a9517e17d514a267b72c2868e29bfb3d980fa7
michaal-studia <michaal.polaa@gmail.com>
2025-05-22 17:36:20 +0200

    Poprawiono wyglad UI

---
+++ BooTea-game/Assets/QuestController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+
+public class QuestController : MonoBehaviour
+{
+    public static QuestController Instance { get; private set; }
+    public List<QuestProgress> activateQuests = new();
+    private QuestUI questUI;
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+        
+        questUI = FindObjectOfType<QuestUI>();
+    }
+
+    public void AcceptQuest(Quest quest)
+    {
+        if (IsQuestActive(quest.QuestID)) return;
+
+        activateQuests.Add(new QuestProgress(quest));
+
+        questUI.UpdateQuestUI(); 
+    }
+
+    public bool IsQuestActive(string questID)=> activateQuests.Exists(q => q.QuestID == questID);
+}
